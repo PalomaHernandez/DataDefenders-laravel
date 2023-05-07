d11d041f5de8e0ea10cc224a849981b96b0205b0
Zeke <erb1995@gmail.com>
2023-05-07 11:35:23 -0300

    Removed outdated comment.
    This comment was here to explain what this relationship would do in SQL. It is not necessary anymore.

--- app/Models/Request.php
+++ app/Models/Request.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
@@ -27,7 +28,7 @@ class Request extends Model {
 	];
 
 	public function user():BelongsTo{
-		return $this->belongsTo(User::class); // select * from users where id=? LIMIT 1;
+		return $this->belongsTo(User::class);
 	}
 
 	public function offer():MorphTo{
