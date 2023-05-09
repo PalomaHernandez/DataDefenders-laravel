7c84dc2559991c1c4a7488f2af784aba46ae0383
PalomaHernandez <palohernandez01@gmail.com>
2023-05-08 23:23:25 -0300

    Added all API routes for Swagger

--- routes/api/majors.php
+++ routes/api/majors.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\MajorController;
 
 Route::get('/', [MajorController::class, 'index']);
 Route::post('/store', [MajorController::class, 'store']);
-Route::patch('/{majorId}/find', [MajorController::class, 'find']);
+Route::get('/{majorId}/find', [MajorController::class, 'find']);
 Route::patch('/{majorId}/update', [MajorController::class, 'update']);
 Route::delete('/{majorId}/delete', [MajorController::class, 'delete']);
